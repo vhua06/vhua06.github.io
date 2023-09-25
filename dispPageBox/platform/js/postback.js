1103043b97cf8bd93c3c3466211081bcbce98c03
vhua06 <vhua06@gmail.com>
2023-09-25 11:27:12 +0800

    Remove Old

--- dispPageBox/platform/js/postback.js
+++
@@ -1,5 +0,0 @@
-var theForm = document.forms['aspnetForm'];if (!theForm) {theForm = document.aspnetForm;};function __doPostBack(eventTarget, eventArgument) {if (!theForm.onsubmit || (theForm.onsubmit() != false)) {theForm.__EVENTTARGET.value = eventTarget;theForm.__EVENTARGUMENT.value = eventArgument;theForm.submit();};};
-
-function TreeView_PopulateNodeDoCallBack(context,param) {
-        WebForm_DoCallback(context.data.treeViewID,param,TreeView_ProcessNodeData,context,TreeView_ProcessNodeData,false);
-}
