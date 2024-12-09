ffdef508c011410506a0714f5e662965dbe5d3ab
vhua06 <vhua06@gmail.com>
2024-12-09 14:31:29 +0800

    U ij

--- javascripts/sliding.js
+++ javascripts/sliding.js
@@ -36,65 +36,65 @@ function sliding() {
   }, 1000);
 }
 
-
 $(function () {
+  
   $( "input[name='startAt']" )
   .datepicker({
       dateFormat: "yy/mm/dd", 
       minDate: new Date(2020, 0, 4),
       maxDate: new Date(2020, 0, 6)
+  });  
+  $('.timeSelect').clockpicker({
+    autoclose: true,
   });
-});
-$('.timeSelect').clockpicker({
-  autoclose: true,
-});
-$('#search').click(function () {
-  $('.timeOrder').css('display', 'none');
+  $('#search').click(function () {
+    $('.timeOrder').css('display', 'none');
 
-  const startAt = `${$('input[name="startAt"').val()}`; 
-  const dateList = startAt.split('/');
-  const year = dateList[0];
-  const month = dateList[1];
-  const day = dateList[2];
-  const timeType = $("input[name='timeType']:checked").val();    
-  const time = `${$('input[name="time"').val()}`;
-  var timeList = time.split(':');
-  var hour = timeList[0];
-  var minute = timeList[1];
-  
-  var dataLen = 0; //資料長度
-  if(minute != '00') {
-    dataLen = Number(hour) + 1;
-  }
-  else {dataLen = Number(hour);} 
-  if (dataLen > 23) { dataLen = 23; } 
-  
-  var content = "";
-  var str1 = ""; //建立一個空字串，要將資料填入的
-  var str2 = ""; //建立一個空字串，要將資料填入的
-  var pmEl = $("#pm");
-  var metEl = $("#met");
-
-  // init innerHTML TAG
-  pmEl.html(str1);
-  metEl.html(str2);
-
-  if(timeType == 'hour') { // timeType: hour
-    for (var i = 0; i < dataLen; i++) {
-      content = "<li><img src='./images/PM2.5/" + year + "_M" + String(month).padStart(2, '0') + "_D" + String(day).padStart(2, '0') + "_" + String(i).padStart(2, '0') + "00(UTC+0800)_L00_TPM2.5_1HR_CONC.JPG'/></li>"; //組字串
-      str1 += content; // 透過加總把 變數 content 的內容回傳到 str 中
+    const startAt = `${$('input[name="startAt"').val()}`; 
+    const dateList = startAt.split('/');
+    const year = dateList[0];
+    const month = dateList[1];
+    const day = dateList[2];
+    const timeType = $("input[name='timeType']:checked").val();    
+    const time = `${$('input[name="time"').val()}`;
+    var timeList = time.split(':');
+    var hour = timeList[0];
+    var minute = timeList[1];
+    
+    var dataLen = 0; //資料長度
+    if(minute != '00') {
+      dataLen = Number(hour) + 1;
     }
-    for (var i = 0; i < dataLen; i++) {
-      content = "<li><img src='./images/MET/" + year + "_M" + String(month).padStart(2, '0') + "_D" + String(day).padStart(2, '0') + "_" + String(i).padStart(2, '0') + "00(UTC+0800)_L01_1HR_Wind.JPG'/></li>"; //組字串
-      str2 += content; // 透過加總把 變數 content 的內容回傳到 str 中
-    }
+    else {dataLen = Number(hour);} 
+    if (dataLen > 23) { dataLen = 23; } 
+    
+    var content = "";
+    var str1 = ""; //建立一個空字串，要將資料填入的
+    var str2 = ""; //建立一個空字串，要將資料填入的
+    var pmEl = $("#pm");
+    var metEl = $("#met");
+
+    // init innerHTML TAG
     pmEl.html(str1);
     metEl.html(str2);
-  }
-  else { // timeType: day
-    pmEl.html("<li><img src='./images/PM2.5/" + year + "_M" + String(month).padStart(2, '0') + "_D" + String(day).padStart(2, '0') + "_0000(UTC+0800)_L00_TPM2.5_24HR_CONC.JPG'/></li>");
-    metEl.html("<li>&nbsp;</li>");
-  }
 
-  sliding();
+    if(timeType == 'hour') { // timeType: hour
+      for (var i = 0; i < dataLen; i++) {
+        content = "<li><img src='./images/PM2.5/" + year + "_M" + String(month).padStart(2, '0') + "_D" + String(day).padStart(2, '0') + "_" + String(i).padStart(2, '0') + "00(UTC+0800)_L00_TPM2.5_1HR_CONC.JPG'/></li>"; //組字串
+        str1 += content; // 透過加總把 變數 content 的內容回傳到 str 中
+      }
+      for (var i = 0; i < dataLen; i++) {
+        content = "<li><img src='./images/MET/" + year + "_M" + String(month).padStart(2, '0') + "_D" + String(day).padStart(2, '0') + "_" + String(i).padStart(2, '0') + "00(UTC+0800)_L01_1HR_Wind.JPG'/></li>"; //組字串
+        str2 += content; // 透過加總把 變數 content 的內容回傳到 str 中
+      }
+      pmEl.html(str1);
+      metEl.html(str2);
+    }
+    else { // timeType: day
+      pmEl.html("<li><img src='./images/PM2.5/" + year + "_M" + String(month).padStart(2, '0') + "_D" + String(day).padStart(2, '0') + "_0000(UTC+0800)_L00_TPM2.5_24HR_CONC.JPG'/></li>");
+      metEl.html("<li>&nbsp;</li>");
+    }
+
+    sliding();
+  });
 });
